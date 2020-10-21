6c9a1655cee1466b834dfaea6d6f56f627c5cbf2
侯粤嘉 <2298090131@qq.com>
2020-10-21 19:05:13 +0800

    网盘项目初始化，引入全局样式，自定义图标库，配置底部导航

---
+++ cloud-disk/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*引入官方样式库*/
+	@import url("/common/uni.css");
+	/*引入本项目公共样式库*/
+	@import url("/common/common.css");
+	/*引入通用free样式库*/
+	@import url("/common/free.css");
+</style>
