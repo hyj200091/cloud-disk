fb2f6300f142be5e33a79955fef97d04e1960105
侯粤嘉 <2298090131@qq.com>
2020-10-21 20:02:50 +0800

    封装自定义导航栏，使用solt来传递自定义导航左右中的内容，并固定其位置

--- cloud-disk/pages/index/index.vue
+++ cloud-disk/pages/index/index.vue
@@ -1,33 +1,31 @@
 <template>
 	<view>
 		<!-- 自定义导航栏 -->
-		<view>
-			<uni-status-bar></uni-status-bar>
-			<view style="height: 44px;" class="flex border-bottom align-center">
-				<view class="flex-1 flex">
-					<text class="font-md ml-3">首页</text>
-				</view>
-				<view class="flex-1 flex justify-center"></view>
-				<view class="flex-1 flex justify-end">
-					<view style="width: 60rpx; height: 60rpx;
-					" class="flex align-center justify-center bg-light rounded-circle mr-3">
+		<nav-bar>
+			<text slot="left" class="font-md ml-3">首页</text>
+			<template slot="right">
+				<view style="width: 60rpx; height: 60rpx;"
+				class="flex align-center justify-center bg-light rounded-circle mr-3"
+				>
 					<text class="iconfont icon-zengjia"></text>
-					</view>
-					<view style="width: 60rpx; height: 60rpx;"
-					class="flex align-center justify-center bg-light rounded-circle mr-3">
-					<text class="iconfont icon-gengduo"></text>
-					</view>
 				</view>
-			</view>
-		</view>
+				<view style="width: 60rpx; height: 60rpx;"
+				 class="flex align-center justify-center bg-light rounded-circle mr-3"
+				 >
+				<text class="iconfont icon-gengduo"></text>
+				</view>
+			</template>
+		</nav-bar>
+		
+		<view class="bg-info" style="height: 1000px">123</view>
 	</view>
 </template>
 
 <script>
-	import uniStatusBar from '../../components/uni-ui/uni-status-bar/uni-status-bar.vue'
+import navBar from '@/components/common/nav-bar.vue'
 	export default {
 		components:{
-			uniStatusBar
+			navBar
 		},
 		data() {
 			return {
